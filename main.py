ce5b3a488b497e62bcb463a356af00e038a7284f
qurel <fda>
2023-09-04 23:44:10 +0800

    实现加载子页面前的缓冲

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import sys, os
 from typing import Optional
 
-import PySide6.QtCore
+from PySide6.QtCore import QThread, Signal
 
 sys.path.append(os.getcwd())
 
 from ui.design.Ui_select_function import Ui_Form
-from PySide6.QtWidgets import QWidget, QApplication
+from PySide6.QtWidgets import QWidget, QApplication, QLabel, QHBoxLayout
+from PySide6.QtGui import QMovie
 from hook import UncaughtHook
 
+class BackgroudImport(QThread):
+    signal = Signal(str)
+    def __init__(self) -> None:
+        super().__init__()
+
+    def run(self) -> None:
+        import pyttsx3, nltk
+        self.signal.emit("")
+
+class LoadingLabel(QLabel):
+    
+    def sizeHint(self):
+        return self.movie().scaledSize()
 
 class Entrance(QWidget, Ui_Form):
 
@@ -22,12 +36,27 @@ class Entrance(QWidget, Ui_Form):
     
     def confirm_btn_callback(self):
         index = self.select_funcion_combo.currentIndex()
+        movie = QMovie('./resources/loading.gif')
+        label = LoadingLabel(self)
+        label.setMovie(movie)
+        label.adjustSize()
+        label2 = QLabel("LOADING...", self)
+        self.horizontalLayout_loading = QHBoxLayout()
+        self.horizontalLayout_loading.addWidget(label)
+        self.horizontalLayout_loading.addWidget(label2)
+
         if index == 0:
             pass
         else:
-            import pyttsx3, nltk
-            engine = pyttsx3.init()
-            print(123)
+            self.thread = BackgroudImport()
+            self.thread.signal.connect(self.import_success_callback)
+            self.thread.start()
+            self.verticalLayout.addLayout(self.horizontalLayout_loading)
+            movie.start()
+            
+
+    def import_success_callback(self):
+        print('log success...')
 
 
 
--- ui/design/Ui_select_function.py
+++ ui/design/Ui_select_function.py
@@ -15,25 +15,55 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QComboBox, QLabel, QPushButton,
-    QSizePolicy, QWidget)
+from PySide6.QtWidgets import (QApplication, QComboBox, QHBoxLayout, QLabel,
+    QLayout, QPushButton, QSizePolicy, QVBoxLayout,
+    QWidget)
 
 class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
         Form.resize(336, 151)
-        self.select_funcion_combo = QComboBox(Form)
+        self.widget = QWidget(Form)
+        self.widget.setObjectName(u"widget")
+        self.widget.setGeometry(QRect(70, 30, 180, 81))
+        self.verticalLayout = QVBoxLayout(self.widget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setSizeConstraint(QLayout.SetFixedSize)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.label = QLabel(self.widget)
+        self.label.setObjectName(u"label")
+
+        self.horizontalLayout.addWidget(self.label)
+
+        self.select_funcion_combo = QComboBox(self.widget)
         self.select_funcion_combo.addItem("")
         self.select_funcion_combo.addItem("")
         self.select_funcion_combo.setObjectName(u"select_funcion_combo")
-        self.select_funcion_combo.setGeometry(QRect(130, 40, 141, 22))
-        self.label = QLabel(Form)
-        self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(60, 40, 71, 16))
-        self.pushButton = QPushButton(Form)
+
+        self.horizontalLayout.addWidget(self.select_funcion_combo)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.pushButton = QPushButton(self.widget)
         self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(110, 90, 75, 24))
+        sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy)
+        self.pushButton.setLayoutDirection(Qt.LeftToRight)
+
+        self.horizontalLayout_2.addWidget(self.pushButton)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+
 
         self.retranslateUi(Form)
 
@@ -42,10 +72,10 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"OfflineSubtitle", None))
+        self.label.setText(QCoreApplication.translate("Form", u"\u9009\u62e9\u529f\u80fd", None))
         self.select_funcion_combo.setItemText(0, QCoreApplication.translate("Form", u"gpt\u8bed\u97f3\u804a\u5929", None))
         self.select_funcion_combo.setItemText(1, QCoreApplication.translate("Form", u"\u7cfb\u7edf\u58f0\u97f3\u79bb\u7ebf\u5b57\u5e55", None))
 
-        self.label.setText(QCoreApplication.translate("Form", u"\u9009\u62e9\u529f\u80fd", None))
         self.pushButton.setText(QCoreApplication.translate("Form", u"\u786e\u5b9a", None))
     # retranslateUi
 
