d3d9aae6b27b9da8ef1eaac794050a40cdc7477b
QureL <fda>
2023-09-05 23:54:17 +0800

    添加gpt setting

--- config/config.py
+++ config/config.py
@@ -6,21 +6,8 @@ import const
 config_base_path = os.path.join(os.path.expanduser("~"), 'AppData', 'Local',
                                 f'{const.APP_NAME}.ini')
 
-class SubtitleConfig:
-
-    group = "SUBTITLE/"
-
-    def __init__(self) -> None:
-        self.setting = QSettings(config_base_path, QSettings.IniFormat)
-    
-    def get_config(self, key:str):
-        return self.setting.value(self.group + key)
-    
-    def set_config(self, key:str, value):
-        self.setting.setValue(self.group + key, value)
-
-class GPTChatConfig:
-    group = "GPT/"
+class ConfigBase:
+    group = ""
 
     def __init__(self) -> None:
         self.setting = QSettings(config_base_path, QSettings.IniFormat)
@@ -29,4 +16,17 @@ class GPTChatConfig:
         return self.setting.value(self.group + key)
     
     def set_config(self, key:str, value):
+        if value == None: return
         self.setting.setValue(self.group + key, value)
+
+class SubtitleConfig(ConfigBase):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.group = "SUBTITLE/"
+
+class GPTChatConfig(ConfigBase):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.group = "GPT/"
